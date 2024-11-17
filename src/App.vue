395e9b80efca4f89b8a33b59689440365864b9a7
alwin (mac) <alwindoudou@gmail.com>
2024-11-17 19:01:46 +0100

    Modification UI

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,12 @@ export default {
 </script>
 
 <template>
-    <div class="flex grow h-dvh flex-col">
+    <div class="flex grow h-dvh flex-col gap-4">
         <Add_sujet @created="sujet_created" v-if="$route.name != 'login'" @close="this.open_add_sujet = false" :on_open="this.open_add_sujet" />
         <div class="overflow-auto flex" :class="$route.name == 'login' ? 'grow' : ''">
             <RouterView :reload="reload" ref="routerView" />
         </div>
-        <nav v-if="$route.name != 'login'" class="flex mt-4 bg-secondary grow w-full mt-auto max-h-[100px] min-h-[75px] h-52">
+        <nav v-if="$route.name != 'login'" class="flex bg-secondary grow w-full mt-auto max-h-[100px] min-h-[75px] h-52">
             <Bottom_navbar @add_sujet="add_sujet_dialog" />
         </nav>
     </div>
